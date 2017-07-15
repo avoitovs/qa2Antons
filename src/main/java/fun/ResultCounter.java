f5dd21e6b77bcc2b6e8cc8afd5293c06bccf05c7
Antons Voitovs <antooshaa@gmail.com>
2017-07-16 02:27:47 +0300

    qa2 System test backend

--- src/main/java/fun/ResultCounter.java
+++ src/main/java/fun/ResultCounter.java
@@ -20,9 +20,10 @@ public class ResultCounter {
 
         for (Seat seat : player.getSeats()){
 
-            if (seat.getScore()<dealer.getScore()&seat.getScore()<22){
+            if (seat.getScore()>dealer.getScore() & seat.getScore()<22 |
+                    seat.getScore()<22 & dealer.getScore()>21){
                 System.out.println("Seat number "+seatNumber+" won!");
-            } else if (seat.getScore()==dealer.getScore()&seat.getScore()<22){
+            } else if (seat.getScore()==dealer.getScore()&&seat.getScore()<22){
                 System.out.println("Tie for seat number "+seatNumber);
             } else {
                 System.out.println("Dealer won against seat "+seatNumber);
